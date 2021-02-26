7dcdbd7d7ecc968e372b2efc040f6da18599163e
Aman Jain <aman@lucentinnovation.com>
2021-02-26 19:09:29 +0530

    responsive try

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,8 +1,11 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "mobile-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "mobile-nav";
-  }
-}
+$(".mobile-three-bars .toggle").click(function () {
+  
+  $(".mobile-center-content .ullist").slideToggle();
+// $("ul.ul-list > li").slideToggle();
+});
+
+//for clicking on li list
+
+$(".new-arrivals .toggle-list").click(function(){
+  $("list-all-new-arrivals-mobile").slideToggle();
+})
